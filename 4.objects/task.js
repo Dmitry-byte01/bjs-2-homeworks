8a6599673f34edd0b4d1f5951114a46bb2c2259b
DM <dmatig01@mail.ru>
2025-11-23 17:56:36 +0300

    Выполнено задание 4.objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,38 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (!this.marks) {
+    return; // студент отчислен — нет массива marks
+  }
+  this.marks.push(...marks);
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (!this.marks || this.marks.length === 0) {
+    return 0;
+  }
+  const sum = this.marks.reduce((acc, val) => acc + val, 0);
+  return sum / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
+}
+
+// Для Jest и GitHub Pages
+if (typeof module !== 'undefined') {
+  module.exports = {
+    Student
+  };
 }
