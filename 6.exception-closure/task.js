c080ad21cd4ff7d91c0905870272964544e39718
DM <dmatig01@mail.ru>
2025-12-04 16:04:44 +0300

    Выполнено задание 6.exception-closure

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,82 @@
-﻿
+﻿// ======================
+// ЗАДАЧА 1. Форматтер чисел
+// ======================
+
+function parseCount(value) {
+  const parsed = Number.parseFloat(value);
+
+  if (Number.isNaN(parsed)) {
+    throw new Error("Невалидное значение");
+  }
+
+  return parsed;
+}
+
+function validateCount(value) {
+  try {
+    return parseCount(value);
+  } catch (error) {
+    return error;
+  }
+}
+
+// ======================
+// ЗАДАЧА 2. Треугольник
+// ======================
+
+class Triangle {
+  constructor(a, b, c) {
+    // правило существования треугольника
+    if (a + b <= c || a + c <= b || b + c <= a) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    }
+
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+  get perimeter() {
+    return this.a + this.b + this.c;
+  }
+
+  get area() {
+    const p = this.perimeter / 2;
+    const area = Math.sqrt(
+      p *
+      (p - this.a) *
+      (p - this.b) *
+      (p - this.c)
+    );
+
+    return Number(area.toFixed(3));
+  }
+}
+
+// ======================
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
+  } catch {
+    return {
+      get area() {
+        return "Ошибка! Треугольник не существует";
+      },
+      get perimeter() {
+        return "Ошибка! Треугольник не существует";
+      }
+    };
+  }
+}
+
+// ======================
+
+if (typeof module !== "undefined") {
+  module.exports = {
+    parseCount,
+    validateCount,
+    Triangle,
+    getTriangle
+  };
+}
